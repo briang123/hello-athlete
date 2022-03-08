015d63f822485c457e82e792faebeef670e4e732
Brian G <bgaines@npauctions.com>
2022-03-08 00:58:54 -0800

    more udpates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { calculatePace, fake5kRaceResults } from 'athlete-calculations';
 
-function App() {
+export const App = () => {
+  function fakeData() {
+    return {
+      fetch: () => fake5kRaceResults,
+    };
+  }
+
+  function pace() {
+    return {
+      processResults: (data) =>
+        data.map((resultItem) => {
+          const { distance, units, hours, minutes, seconds } = resultItem;
+          return {
+            ...resultItem,
+            pace: calculatePace({
+              distance: { traveled: distance, units },
+              time: {
+                hours,
+                minutes,
+                seconds,
+                units,
+              },
+              format: '%M:%SS',
+            }).pace.formatted,
+          };
+        }),
+    };
+  }
+
+  const faker = fakeData();
+  const fakeResults = faker.fetch();
+  const p = pace();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        height: '100vh',
+        width: '100vw',
+        color: 'hsla(219, 95%, 76%, 1.0)',
+        backgroundColor: 'hsla(209, 100%, 9%, 1.0)',
+        display: 'flex',
+        alignContent: 'flex-start',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        fontSize: '1.3rem',
+        padding: 20,
+        margin: 0,
+        gap: 40,
+      }}
+    >
+      {p.processResults(fakeResults).map((result) => (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            textAlign: 'center',
+            border: '3px solid hsla(205, 83%, 16%, 1.0)',
+            borderRadius: '50%',
+            height: '200px',
+            width: '200px',
+            gap: '20px',
+            padding: 0,
+          }}
+          key={result.bib}
         >
-          Learn React
-        </a>
-      </header>
+          <div>{result.bib}</div>
+          <div>{result.name}</div>
+          <div>{result.results}</div>
+          <div>{result.pace}</div>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
